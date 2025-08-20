223a741f48cd84b251c5447b536ca85bbca9f5c0
SumedhaKS <kssumedha24@gmail.com>
2025-08-20 23:00:31 +0530

    init

---
+++ work_station/backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 5001;
+const mainRouter = require("./routes/index")
+
+app.use(express.json())
+app.use(cors())
+app.use('/api/v1/', mainRouter)
+
+
+app.listen(PORT , ()=> console.log(`Server running on port: ${PORT}`))
---
+++ work_station/backend/routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const userRouter = require("./user.js")
+const modelRouter = require("./model.js")
+
+router.use('/user', userRouter);
+app.use('/model', modelRouter)
+
+module.exports = router;
