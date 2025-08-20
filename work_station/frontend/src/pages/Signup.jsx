93fd640e73f215346a7ea00a8ab6bf6b6e854f29
SumedhaKS <kssumedha24@gmail.com>
2025-08-20 23:36:46 +0530

    sample

---
+++ work_station/frontend/src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import Button from "../components/Button"
+export default function Signup(){
+
+    return <div>
+        Signup page
+        <Button onClick={()=>{
+
+        }} >Submit</Button>
+    </div>
+}
