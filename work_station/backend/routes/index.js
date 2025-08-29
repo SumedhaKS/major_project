d913cafc9050e61113fab078d0d2d461782c799d
Kushal Kumar <kushalkumar.bp@gmail.com>
2025-08-30 00:38:09 +0530

    1.changed users schema

--- work_station/backend/routes/index.js
+++ work_station/backend/routes/index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userRouter = require("./user.js")
 const modelRouter = require("./model.js")
-const login_register = require('./login_register.js');
 
 
 
 router.use('/user', userRouter);
 router.use('/model', modelRouter);
-router.use('/', login_register)
 
 module.exports = router;
