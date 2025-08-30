e3f117a7e07e85fccb1f8701dc22f61341aded78
SumedhaKS <118447724+SumedhaKS@users.noreply.github.com>
2025-08-30 20:07:20 +0530

    Update auth.js

--- work_station/backend/middleware/auth.js
+++ work_station/backend/middleware/auth.js
@@ -4,17 +4,21 @@ const {verifyToken} = require('./jwt');
 //function is working -> tested
 module.exports = function authMiddleware(req, res, next) {
     try{
-        const headder = req.headers.authorization || "";
-        const [schema , token] = headder.split(" ");
+        const header = req.headers.authorization || "";
+        const [schema , token] = header.split(" ");
         if(schema != "Bearer" || !token){
-            res.json({"message":"token not found or invalid token"}).status(401);
+            return res.json({
+                "message":"token not found or invalid token"
+            }).status(401);
         }
         const decoded = verifyToken(token);
         req.user = decoded;
         return next(); 
     }
     catch(err){
-        console.log(`incalid token : error during authorization. Error: ${err}`);
-        res.json({"message":"invalid token or expired token"}).status(401);
+        console.error(`invalid token : error during authorization. Error: ${err}`);
+        res.json({
+            "message":"invalid token or expired token"
+        }).status(401);
     }
 }
