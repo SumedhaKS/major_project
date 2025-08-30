70aabb8939cfb9cee0ebdd34321659ea05b0012a
SumedhaKS <118447724+SumedhaKS@users.noreply.github.com>
2025-08-30 20:09:56 +0530

    Update index.js

--- work_station/backend/routes/index.js
+++ work_station/backend/routes/index.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const userRouter = require("./user.js")
 const modelRouter = require("./model.js")
 
-
-
 router.use('/user', userRouter);
 router.use('/model', modelRouter);
 
