ddcfb6967565732aec0a59dc34856804e4afed39
SumedhaKS <kssumedha24@gmail.com>
2025-09-14 23:43:05 +0530

    working

--- work_station/backend/routes/model.js
+++ work_station/backend/routes/model.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
+
 const authMiddleware = require("../middleware/auth");
-const prisma = require("../db/client");
 const getPatientID = require("../middleware/getPatientID");
 const generateXrayID = require("../middleware/generateXrayId");
 const uploadWithRollback = require("../middleware/multer");
+
+const prisma = require("../db/client");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const FormData = require("form-data")
 
 router.get('/', (req, res) => {
     res.json({
@@ -14,30 +19,67 @@ router.get('/', (req, res) => {
 })
 
 /* 
-req 
-{
+req body {
     patientID ,
     token ,
     image
 }
 */
 
+/*
+    => * images/uploads and images/analyzed can have duplicate images. ie, redundant images. 
+    TODO: hash images and store hash value, compare hash on req
+           - if found use available image
+           - else upload to storage and normal flow 
+*/
 
 router.post("/predict", authMiddleware, getPatientID, generateXrayID, uploadWithRollback, async (req, res) => {
-    // Note: Update XrayID and filepath in DB with actual values
-    // req.xID => xrayId   &&   req.file.path => filepath (maybe)
+
     console.log(req.file);
     console.log(req.xID);
-    await prisma.xRay.update({
-        where: { xrayId: req.xID },
-        data: {
-            filePath: `/images/uploads/${req.file.filename}`
-        }
-    })
-    return res.json({
-        msg: "All middlewares passed"
-    })
+    try {
+        await prisma.xRay.update({
+            where: { xrayId: req.xID },
+            data: {
+                filePath: `/images/uploads/${req.file.filename}`
+            }
+        })
 
+        const form = new FormData()
+        form.append("file", fs.createReadStream(req.file.path))
+
+        const modelResponse = await axios.post("http://localhost:8000/analyze", form, {
+            headers: form.getHeaders(),                             // Returns a shallow copy of the current outgoing headers
+            responseType: "arraybuffer"
+        });
+
+        if (!modelResponse) {
+            return res.status(500).json({
+                msg: "Failed to analyze"
+            })
+        }
+
+        const analyzedFilename = `${req.xID}-analyzed.png`;
+        const getAnalyzedDir = path.resolve("public/images/analyzed");
+        const analyzedPath = path.join(getAnalyzedDir, analyzedFilename);
+
+        await fs.promises.writeFile(analyzedPath, modelResponse.data);
+
+        await prisma.xRay.update({
+            where: { xrayId: req.xID },
+            data: {
+                analyzedPath: analyzedPath
+            }
+        })
+
+        return res.sendFile(analyzedPath);
+
+    } catch (err) {
+        console.error("Error occurred: ", err);
+        return res.status(500).json({
+            msg: "Prediction failed"
+        })
+    }
 
 })
 
